ebb383cd7c4a6966bcd60c2b563062761ee63277
Christina Merlin <81285621+Christinamerlin@users.noreply.github.com>
2023-06-13 20:51:52 +0530

    Create count pairs.py

---
+++ count pairs.py
@@ -0,0 +1,22 @@
+def solution(h, n, k):
+   cnt=0
+   d={}
+   for i in h:
+       if i+k in d:
+           cnt+=d[i+k]
+       if i-k in d:
+            cnt+=d[i-k]
+       d[i]=d.get(i,0)+1
+   return cnt
+            
+
+def main():
+    nk = input().split(" ")
+    n = int(nk[0])
+    k = int(nk[1])
+    a = input().split(" ")
+    for i in range(n):
+        a[i] = int(a[i])
+    print(solution(a, n, k))
+
+main()   
